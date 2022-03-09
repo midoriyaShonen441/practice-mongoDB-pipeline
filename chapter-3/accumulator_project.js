ee8bb49241124150b387f762db7c4f17c0f5429e
khunnatta <61150735+khunnatta@users.noreply.github.com>
2022-03-09 21:58:02 +0700

    add chapter 3

---
+++ chapter-3/accumulator_project.js
@@ -0,0 +1,81 @@
+// run to get a view of the document schema
+db.icecream_data.findOne()
+
+// using $reduce to get the highest temperature
+db.icecream_data.aggregate([
+  {
+    "$project": {
+      "_id": 0,
+      "max_high": {
+        "$reduce": {
+          "input": "$trends",
+          "initialValue": -Infinity,
+          // include data
+          "in": {
+            // use condition
+            "$cond": [
+              // avg_high_temp > value if true show avg_high_tmp if not show value
+              { "$gt": ["$$this.avg_high_tmp", "$$value"] }, // $$ is temporary variable use only in $reduce expression
+              "$$this.avg_high_tmp",
+              "$$value"
+            ]
+          }
+        }
+      }
+    }
+  }
+])
+
+// performing the inverse, grabbing the lowest temperature
+db.icecream_data.aggregate([
+  {
+    "$project": {
+      "_id": 0,
+      "min_low": {
+        "$reduce": {
+          "input": "$trends",
+          "initialValue": Infinity,
+          "in": {
+            "$cond": [
+              { "$lt": ["$$this.avg_low_tmp", "$$value"] },
+              "$$this.avg_low_tmp",
+              "$$value"
+            ]
+          }
+        }
+      }
+    }
+  }
+])
+
+// note that these two operations can be done with the following operations can
+// be done more simply. The following two expressions are functionally identical
+
+db.icecream_data.aggregate([
+  { "$project": { "_id": 0, "max_high": { "$max": "$trends.avg_high_tmp" } } }
+])
+
+db.icecream_data.aggregate([
+  { "$project": { "_id": 0, "min_low": { "$min": "$trends.avg_low_tmp" } } }
+])
+
+// getting the average and standard deviations of the consumer price index
+db.icecream_data.aggregate([
+  {
+    "$project": {
+      "_id": 0,
+      "average_cpi": { "$avg": "$trends.icecream_cpi" },
+      "cpi_deviation": { "$stdDevPop": "$trends.icecream_cpi" }
+    }
+  }
+])
+
+// using the $sum expression to get total yearly sales
+db.icecream_data.aggregate([
+  {
+    "$project": {
+      "_id": 0,
+      "yearly_sales (millions)": { "$sum": "$trends.icecream_sales_in_millions" }
+    }
+  }
+])
